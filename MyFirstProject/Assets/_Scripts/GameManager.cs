b8b7c3c128439c4bfe601422979e97ef5a2417ed
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-06-11 23:54:30 +0200

    GameManager

--- MyFirstProject/Assets/_Scripts/GameManager.cs
+++ MyFirstProject/Assets/_Scripts/GameManager.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private int itemsCollected = 0;//Items recogidos por el jugador
-    private int playerHP = 100;//Vida del player
+    
+    private int playerHP = 3;//Vida del player
+    public int HP
+    {
+        get { return playerHP; }
+        set
+        {
+            if (value >= 0 && value <= 3)
+            {
+                playerHP = value;
+            }
+            Debug.LogFormat("Vidas: {0}", playerHP);
+        }
 
+    }
+
+    private int itemsCollected = 0;//Items recogidos por el jugador
+    public int Items
+    {
+        get { return itemsCollected;  }
+        set 
+        {
+            if (value >= 0)
+            {
+                itemsCollected = value;
+            }
+            Debug.LogFormat("Items: {0}", itemsCollected);
+        }
+    }
 
 
     // Start is called before the first frame update
--- MyFirstProject/Assets/_Scripts/ItemPickup.cs
+++ MyFirstProject/Assets/_Scripts/ItemPickup.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
+    private GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "Player")
@@ -11,6 +18,8 @@ public class ItemPickup : MonoBehaviour
             //El item desaparece porque el player lo ha recogido
             Destroy(transform.parent.gameObject);
             Debug.Log("Item recogido");
+
+            gameManager.Items++;
         }
     }
 }
