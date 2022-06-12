65491ec6802189e6ba66cfa174b7edd0e05ff9f0
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-06-12 20:50:20 +0200

    Configuración NavMesh

--- MyFirstProject/Assets/_Scripts/GameManager.cs
+++ MyFirstProject/Assets/_Scripts/GameManager.cs
@@ -1,12 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField]//Textos que se mostrarán en la UI
+    private string labelText =
+        "¡Recolecta todos los ítems y consigue la victoria!";
+
+    [SerializeField] private int maxItems = 3;//Número máximo de ítems
     
     private int playerHP = 3;//Vida del player
-    public int HP
+
+    private bool showWinScreen;//Para saber cuándo se debe mostrar la pantalla de victoria
+
+    public int HP//Propiedad para acceder a la vida del player
     {
         get { return playerHP; }
         set
@@ -21,7 +31,7 @@ public class GameManager : MonoBehaviour
     }
 
     private int itemsCollected = 0;//Items recogidos por el jugador
-    public int Items
+    public int Items//Propiedad para acceder a los items recogidos
     {
         get { return itemsCollected;  }
         set 
@@ -31,19 +41,38 @@ public class GameManager : MonoBehaviour
                 itemsCollected = value;
             }
             Debug.LogFormat("Items: {0}", itemsCollected);
+            if( itemsCollected >= maxItems)//Comprueba si se han recogido todos los ítems
+            {
+                labelText = "¡Has encontrado todos los ítems!!!!";
+                Time.timeScale = 0f;
+                showWinScreen = true;
+            }
+            else
+            {
+                labelText = "Ítem encontrado. Te faltan: " + (maxItems - itemsCollected) +
+                    " ítems.";
+            }
         }
     }
 
 
-    // Start is called before the first frame update
-    void Start()
+    //Uso de la clase GUI para mostrar una UI 
+    private void OnGUI()
     {
-        
+        GUI.Box(new Rect(25, 25, 180, 25), "Vida: " + playerHP.ToString("00")); //Muestra una caja con el número de vidas
+
+        GUI.Box(new Rect(25, 65, 180, 25), "Ítems recogidos: " + itemsCollected.ToString("00"));//Muestra una caja con el número de ítems recogidos
+
+        GUI.Label(new Rect(Screen.width / 2 - 100, Screen.height - 50, 200, 50), labelText);//Muestra un texto flotante con información
+
+        if(showWinScreen)//Cuando el jugador haya vencido
+        {
+            if(GUI.Button(new Rect(Screen.width / 2 - 200, Screen.height / 2 - 100, 400, 200), "¡HAS GANADO!!!!"))//Muestra el botón de victoria y si se pulsa en él
+            {
+                Time.timeScale = 1f;
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);//Recarga la escena actual
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
