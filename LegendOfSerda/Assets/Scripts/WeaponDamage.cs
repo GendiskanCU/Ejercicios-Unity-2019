edfe6a86a0fa5d8fa129805a036a37fa7202a0c6
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-06-20 22:57:26 +0200

    Configurando las armas del player

---
+++ LegendOfSerda/Assets/Scripts/WeaponDamage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Controla el daño que hará el arma sobre los enemigos
+
+public class WeaponDamage : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag.Equals("Enemie"))//Si el arma choca con un enemigo
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
