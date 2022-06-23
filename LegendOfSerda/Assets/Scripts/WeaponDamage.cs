13fade9261d9d5d387a8f77d9c72fe1d48dc2d43
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-06-23 16:54:55 +0200

    Partículas al dañar enemigos

--- LegendOfSerda/Assets/Scripts/WeaponDamage.cs
+++ LegendOfSerda/Assets/Scripts/WeaponDamage.cs
@@ -9,11 +9,26 @@ public class WeaponDamage : MonoBehaviour
     [Tooltip("Cantidad de daño que hace el arma")]
     public int damage; //Daño del arma
 
+    public GameObject bloodAnim;//Efecto de partículas de sangre al golpear a un enemigo
+
+    private GameObject hitPoint;//Punto de aparición del efecto de sangre
+
+    private void Start()
+    {
+        //Localiza el hitpoint dentro de sus hijos
+        hitPoint = transform.Find("HitPoint").gameObject;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.tag.Equals("Enemie"))//Si el arma choca con un enemigo le hará daño
         {
             collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);
+
+            //Y muestra el efecto de sangre, si se ha establecido alguno en la vista diseño
+            //Y además el arma tiene establecido un punto de golpe
+            if(bloodAnim != null && hitPoint != null)
+                Instantiate(bloodAnim, hitPoint.transform.position, hitPoint.transform.rotation);
         }
     }
 }
