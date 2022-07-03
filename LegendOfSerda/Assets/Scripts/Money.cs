4558fe5542e4b6ea6ce5b0cfd77a52f1772dfc56
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2022-07-03 20:46:57 +0200

    Recolección de monedas

---
+++ LegendOfSerda/Assets/Scripts/Money.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Controla las monedas que el player puede recoger
+
+[RequireComponent(typeof(BoxCollider2D))]
+
+public class Money : MonoBehaviour
+{
+    public int value;//Valor de la moneda al recogerla
+
+    private MoneyManager moneyManager;//Manager del total de monedas
+
+
+    private void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Cuando la moneda sea recogida suma su valor al total
+        if (collision.gameObject.name.Equals("Player"))
+        {
+            moneyManager.AddMoney(value);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ LegendOfSerda/Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//Gestiona la cantidad de monedas que ha recogido el player
+
+public class MoneyManager : MonoBehaviour
+{
+    //Cantidad de monedas que tiene el player
+    public int currentMoney;
+    //Texto de la UI donde reflejar las monedas que tiene el player
+    public Text moneyText;
+
+
+    /// <summary>
+    /// Añade al total de monedas recogidas la cantidad indicada
+    /// </summary>
+    /// <param name="moneyCollected"></param>
+    public void AddMoney(int moneyCollected)
+    {
+        currentMoney += moneyCollected;
+
+        moneyText.text = currentMoney.ToString("00000");
+
+        //Actualiza la cantidad en las player prefs
+        PlayerPrefs.SetInt("MONEY", currentMoney);
+    }
+
+    private void Start()
+    {
+        //Si había monedas acumuladas de una sesión previa, se cargan
+        if(PlayerPrefs.HasKey("MONEY"))
+        {
+            currentMoney = PlayerPrefs.GetInt("MONEY");
+        }
+        else//Si no había monedas de sesiones previas
+        {
+            currentMoney = 0;
+            PlayerPrefs.SetInt("MONEY", currentMoney);//Se crea por primera vez el valor
+        }
+
+        //Actualiza la información en la UI
+        moneyText.text = currentMoney.ToString("00000");
+    }
+}
